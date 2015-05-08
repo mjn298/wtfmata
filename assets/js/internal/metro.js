e7e1e974389c514524cc5562689cdecc73bd093e
Mike Nissenbaum <mikenissenbaum@moduscreate.com>
2015-05-08 01:02:51 -0400

    committing

---
+++ assets/js/internal/metro.js
@@ -0,0 +1,122 @@
+/**
+ * Created by mjn on 5/7/15.
+ */
+
+var metroApp = {
+
+    loadMetroAlerts: function () {
+
+        var params = {
+            'api_key': '06ab12b939d14769b2e887c967ccd000',
+        };
+        $.ajax({
+            url: 'https://api.wmata.com/Incidents.svc/json/Incidents?' + $.param(params),
+            type: 'GET'
+        })
+            .done(function (data) {
+                var incidentsArray = [];
+                for (x = 0; x < data.Incidents.length; x++) {
+                    incidentsArray.push(data.Incidents[x].Description);
+                }
+                metroApp.publishAlerts(incidentsArray);
+            })
+            .fail(function () {
+                console.log(error)
+            });
+
+
+    },
+
+    publishAlerts: function (inputs) {
+
+        if (inputs.length == 0) {
+            $("#alerts-div").html("<h3>No Alerts!!! Holy F!</h3>");
+        }
+        else {
+            for (x = 0; x < inputs.length; x++) {
+                $("#alerts-list").append("<li class='alert-item'>" + inputs[x] + "</li>");
+            }
+        }
+    },
+
+
+    listStations: function (color) {
+        $.ajax(color + ".json")
+            .done(function(data){
+
+                var stationsArray = [];
+                for (var x = 0; x < data.Stations.length; x++){
+                    var stationObj = {};
+                    stationObj["station"] = data.Stations[x].Name;
+                    stationObj["code"] = data.Stations[x].Code;
+                    stationsArray.push(stationObj);
+
+                }
+                metroApp.fillMetroSelect(color, stationsArray);
+
+            })
+            .fail(function(){
+                console.log("list stations failed");
+                $("#list-fail").css("display", "inline");
+            })
+
+    },
+
+    fillMetroSelect : function(color, inputArray) {
+        for (x = 0; x < inputArray.length; x ++) {
+            $("#" + color).append("<option style='padding-left: 10px' id='"+inputArray[x].code+"'>"+inputArray[x].station+"</option>")
+        }
+    },
+
+    getArrivalTimesForStation: function (id) {
+        
+    },
+
+    triggerFills : function() {
+        var colors = ["blue", "orange", "green", "red", "yellow", "silver"];
+        for (var x = 0; x < colors.length; x++) {
+            metroApp.listStations(colors[x]);
+        }
+    },
+
+    stationHandler : function() {
+        $('#red').change(function(){
+            var id = $("#red option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+        $('#green').change(function(){
+            var id = $("#green option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+        $('#yellow').change(function(){
+            var id = $("#yellow option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+        $('#blue').change(function(){
+            var id = $("#blue option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+        $('#orange').change(function(){
+            var id = $("#orange option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+        $('#silver').change(function(){
+            var id = $("#silver option:selected").attr("id");
+            metroApp.getArrivalTimesForStation(id);
+            console.log(id);
+        });
+    }
+
+};
+
+
+$(document).ready(function(){
+    metroApp.loadMetroAlerts();
+    metroApp.triggerFills();
+    metroApp.stationHandler();
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mjn on 5/7/15.
+ */
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/assets/js/twitterWidget.js', function(req, res){
+    res.set('Content-Type', 'text/javascript');
+    res.sendfile('/assets/js/twitterWidget/js');
+});
+
+app.use(express.static(__dirname + '/assets'));
+
+app.listen(2988);
+console.log('on 2988');
